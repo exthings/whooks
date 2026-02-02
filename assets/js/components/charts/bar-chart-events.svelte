ca582177e7ac5db0bedd53c07beb30010feb9607
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2026-02-02 19:20:56 -0300

    feat(components): add bar chart events

--- assets/js/components/charts/bar-chart-events.svelte
+++ assets/js/components/charts/bar-chart-events.svelte
@@ -1,38 +1,50 @@
 <script lang="ts">
-  import { scaleBand, scaleUtc } from "d3-scale";
-  import { BarChart, type ChartContextValue } from "layerchart";
+  import type { Analytics } from "$types";
+  import { scaleBand, scaleUtc, scaleTime } from "d3-scale";
+  import { BarChart, type ChartContextValue, Highlight } from "layerchart";
   import * as Chart from "$lib/components/ui/chart/index.js";
   import { cubicInOut } from "svelte/easing";
 
-  const successData = [
-    ...Array.from({ length: 50 }, (_, i) => ({
-      date: new Date(2026, 1, 1, 12, 0, i),
-      value: Math.floor(Math.random() * 200),
-    })),
-  ];
+  type Props = {
+    data: Analytics[];
+    interval: "minute" | "hour" | "day";
+  };
 
-  const failedData = [
-    ...Array.from({ length: 50 }, (_, i) => ({
-      date: new Date(2026, 1, 1, 12, 0, i),
-      value: Math.floor(Math.random() * 100),
-    })),
-  ];
+  const { data, interval = "hour" }: Props = $props();
+
+  const chartData = data.map((item) => ({
+    date: new Date(item.dateTime),
+    value: item.count,
+  }));
 
   const chartConfig = {
     success: { label: "success", color: "var(--color-green-400)" },
-    failed: { label: "failed", color: "var(--color-red-400)" },
   } satisfies Chart.ChartConfig;
 
   let context = $state<ChartContextValue>();
+
+  let labelFormat: Intl.DateTimeFormatOptions = $derived.by(() => {
+    switch (interval) {
+      case "minute":
+        return {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        };
+      case "hour":
+        return { day: "2-digit", hour: "2-digit", minute: "2-digit" };
+      default:
+        return { day: "2-digit", month: "2-digit" };
+    }
+  });
 </script>
 
-<div class="h-full">
+<Chart.Container
+  config={chartConfig}
+  class="h-full w-full pl-10 pr-2 pb-4 pt-4"
+>
   <BarChart
     bind:context
-    grid={true}
-    rule={false}
-    legend={false}
-    xScale={scaleBand().padding(0.25)}
     x="date"
     y="value"
     series={[
@@ -40,21 +52,20 @@
         key: "success",
         label: "Success",
         color: chartConfig.success.color,
-        data: successData,
-      },
-      {
-        key: "failed",
-        label: "Failed",
-        color: chartConfig.failed.color,
-        data: failedData,
+        data: chartData,
       },
     ]}
-    seriesLayout="group"
+    padding={{
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+    }}
     props={{
       bars: {
         stroke: "none",
-        strokeWidth: 0,
         rounded: "none",
+        // use the height of the chart to animate the bars
         initialY: context?.height,
         initialHeight: 0,
         motion: {
@@ -65,14 +76,29 @@
       highlight: { area: { fill: "none" } },
       xAxis: {
         format: (d: Date) => {
-          return d.toLocaleTimeString("en-US", {
+          return d.toLocaleDateString("en-US", labelFormat);
+        },
+        ticks: Math.floor(chartData.length / 6),
+      },
+    }}
+  >
+    {#snippet belowMarks()}
+      <Highlight area={{ class: "fill-muted" }} />
+    {/snippet}
+    {#snippet tooltip()}
+      <Chart.Tooltip
+        nameKey="views"
+        labelFormatter={(v: Date) => {
+          return v.toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+            hour: "2-digit",
             minute: "2-digit",
             second: "2-digit",
           });
-        },
-        ticks: (scale) => scaleUtc(scale.domain(), scale.range()).ticks(),
-      },
-      yAxis: { format: (d) => d },
-    }}
-  ></BarChart>
-</div>
+        }}
+      />
+    {/snippet}
+  </BarChart>
+</Chart.Container>
