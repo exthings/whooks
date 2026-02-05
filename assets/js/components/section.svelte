a5fde51fdcf230493210a86d8e54ca1a076f3687
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2026-02-05 19:00:15 -0300

    feat(componets): add section component

---
+++ assets/js/components/section.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  type Props = {
+    title: string;
+    description?: string;
+    actions?: Snippet;
+    children?: Snippet;
+  };
+
+  const { title, description, actions, children }: Props = $props();
+</script>
+
+<div>
+  <div class="flex items-center justify-between pb-2 min-h-12">
+    <h2 class="font-semibold">{title}</h2>
+    {#if actions}
+      <div>{@render actions()}</div>
+    {/if}
+  </div>
+  {@render children?.()}
+</div>
