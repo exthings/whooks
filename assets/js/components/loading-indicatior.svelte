a47e2a102593a93a2b64ad84d90b02cd61aceb95
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2026-03-27 20:30:29 -0300

    feat(js): add loading indicator component

---
+++ assets/js/components/loading-indicatior.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { LoaderCircleIcon } from "lucide-svelte";
+</script>
+
+<div class="w-full h-20 flex items-center justify-center">
+  <LoaderCircleIcon class="animate-spin" />
+</div>
