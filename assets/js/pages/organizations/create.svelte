a26f9fd1d0579f2c8660fbddd79e6263038e6b12
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2026-03-30 16:18:30 -0300

    feat(js): add create organizations page

---
+++ assets/js/pages/organizations/create.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import * as Empty from "$lib/components/ui/empty/index.js";
+
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Building2Icon } from "lucide-svelte";
+</script>
+
+<Empty.Root class="h-[calc(100vh-1rem)]">
+  <Empty.Header>
+    <Empty.Media variant="icon">
+      <Building2Icon />
+    </Empty.Media>
+    <Empty.Title class="text-2xl">No organization yet</Empty.Title>
+    <Empty.Description class="text-md">
+      You haven't created any organization yet.
+      <br />
+      Get started by creating your first organization.
+    </Empty.Description>
+  </Empty.Header>
+  <Empty.Content>
+    <div class="flex gap-2">
+      <Button>Create organization</Button>
+    </div>
+  </Empty.Content>
+</Empty.Root>
